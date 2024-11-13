6008b5a8ec3afb8aa7df39959c50059f22d127e6
Nicolas Garric <nicolasdfgarric@gmail.com>
2024-11-13 12:09:45 +0100

    Validattions done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,6 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true
   validates :category, presence: true
 
-  validates :category, inclusion: { in: `%w(chinese italian japanese french belgian)`,
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
   message: "%{value} is not a valid category" }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,7 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true
   validates :content, presence: true
+
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
 end
