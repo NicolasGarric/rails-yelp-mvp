c4a363a35c0ea60296e3e618cb1cb79b322c9423
Nicolas Garric <nicolasdfgarric@gmail.com>
2024-11-13 12:19:13 +0100

    Restaurants test created in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Destroying restaurants...'
+Restaurant.destroy_all
+
+Restaurant.create!(name: 'Chez Justine', address: '1 rue Ménilmontant', phone_number: '01 25 26 24 26', category: 'french')
+Restaurant.create!(name: 'Chez Beber', address: '1 rue de Béber', phone_number: '01 35 36 34 36', category: 'belgian')
+Restaurant.create!(name: 'Maxi Tacos 3.0', address: '3 rue du Tacos', phone_number: '01 65 66 64 66', category: 'japanese')
+Restaurant.create!(name: 'Tokyo food', address: '3 avenue de Tokyo', phone_number: '01 00 00 00 01', category: 'italian')
+Restaurant.create!(name: 'Roma Pizza', address: '3 rue de Rome', phone_number: '01 66 66 66 66', category: 'chinese')
+puts 'Creating restaurants...'
+
+Restaurant.count
+puts "#{Restaurant.count} restaurants created :)"
